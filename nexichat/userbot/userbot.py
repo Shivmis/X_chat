4fbf74f22f2cf0b4bbfc1dd990a1edd7a8e8d04f
Shivmis <githubshiv@gmail.com>
2024-11-18 06:59:44 +0530

    Update userbot.py

--- nexichat/userbot/userbot.py
+++ nexichat/userbot/userbot.py
@@ -9,7 +9,7 @@ import config
 class Userbot(Client):
     def __init__(self):
         self.one = Client(
-            name="VIPAss1",
+            name="Shiv",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
@@ -24,10 +24,10 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("THE_VIP_BOY")
-                await self.one.join_chat("THE_VIP_BOY_OP")
-                await self.one.join_chat("TG_FRIENDSS")
-                await self.one.join_chat("VIP_CREATORS")
+                await self.one.join_chat("shivang_xd")
+                await self.one.join_chat("FRIENDS_ZONE_GP")
+                await self.one.join_chat("stickers_Channell")
+                await self.one.join_chat("II_MOON_WORLD_XD")
 
             except:
                 pass
